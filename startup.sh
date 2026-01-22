e53ab1581e4834e9c54ef5faf31a2bd71a457564
Srinivasa moorthy <Srinivasamoorthy2000@gmail.com>
2026-01-22 11:53:04 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "Starting Flask app..."
+echo "Starting Azure Web App..."
 
+# Install dependencies
+echo "Installing dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
+echo "Starting Gunicorn..."
 exec gunicorn app.main:app \
-  --bind 0.0.0.0:${PORT:-8000} \
-  --workers 2 \
-  --timeout 600
+     --bind 0.0.0.0:${PORT:-8000} \
+     --workers 2
