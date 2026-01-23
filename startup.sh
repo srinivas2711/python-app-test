32e42c43eb4e71e720af74400cb43a2e761f99d6
Srinivasa moorthy <Srinivasamoorthy2000@gmail.com>
2026-01-23 11:09:53 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,4 @@ echo "Installing dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
 echo "Starting Gunicorn..."
-exec gunicorn app.main:app \
-     --bind 0.0.0.0:${PORT:-8000} \
-     --workers 2
+exec gunicorn app.main:app --workers 2
